04b15a86b4f8b618fb82491a77e6bab93c663420
KN <ckn.kaien@gmail.com>
2024-10-18 17:28:19 +0800

    Updated data path.

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -1,6 +1,6 @@
 async function getData() {
   try {
-    let res = await fetch("../../data.json");
+    let res = await fetch("./data.json");
     let data = await res.json().then((data) => {
       console.log("successful!");
       return data;
