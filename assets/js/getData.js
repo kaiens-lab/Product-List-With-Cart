9b300bc97115a37bf947656f5bf5ac7c14f1198b
KN Chang <ckn.kaien@gmail.com>
2024-12-12 23:19:53 +0800

    Update getData.js

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -1,13 +1,10 @@
 async function getData() {
-  try {
-    let res = await fetch("./data.json");
-    let data = await res.json().then((data) => {
-      console.log("successful!");
-      return data;
-    });
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        let res = await fetch("./data.json");
+        let data = await res.json(); 
+        console.log("successful!");
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 }
